7e78d2de142970fd0e897155c48f5a3b866f3ddb
carrie <2153074275@qq.com>
2020-06-27 22:01:34 +0800

    在userapp里新增了类视图

--- userapp/views.py
+++ userapp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
 def login(request):
     return HttpResponse("登陆成功")
 
@@ -11,3 +14,6 @@ def index(request):
 def demo(request):
     print("hello world")
     return HttpResponse("本次提交成功")
+
+class UserView(View):
+    pass
